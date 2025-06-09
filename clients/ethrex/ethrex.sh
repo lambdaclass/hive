1cfaab258571a23ef795ed1ccc8300be2a8a1e80
Lucas Rack <lucas.rack@lambdaclass.com>
2025-06-09 13:00:27 -0300

    Use full as defult both clients

--- clients/ethrex/ethrex.sh
+++ clients/ethrex/ethrex.sh
@@ -45,7 +45,7 @@ if [ "$HIVE_NODETYPE" == "snap" ]; then
     FLAGS="$FLAGS --syncmode snap"
 fi
 if [ "$HIVE_NODETYPE" == "" ]; then
-    FLAGS="$FLAGS --syncmode snap"
+    FLAGS="$FLAGS --syncmode full"
 fi
 
 echo "Command flags till now:"
